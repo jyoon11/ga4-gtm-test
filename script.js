df313569d0f421f0fbb2ecd954056dac33fbec3c
James Yoon <jamesyoon@James-M1-MBP.lan>
2024-06-09 12:50:57 -0700

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function gotoSecondPage() {
   window.location.href = 'https://jyoon11.github.io/ga4-gtm-test/second.html';
 }
+
+function trackOne() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZPDGYKBSC6');
+}
